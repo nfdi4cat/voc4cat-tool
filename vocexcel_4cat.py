38b5f14dce69a6e6d45819676893282f0a1f633e
David Linke <david.linke@catalysis.de>
2022-04-05 11:47:59 +0200

    Initial version for a wrapper for VocExcel

---
+++ vocexcel_4cat.py
@@ -0,0 +1,62 @@
+"""
+Add mechanism to extend VocExcel with more commands
+
+The new commands be may be run either before or after VocExcel.
+
+Copyright (c) 2022 David Linke (ORCID: 0000-0002-5898-1820)
+"""
+import argparse
+import sys
+
+from pathlib import Path
+
+from vocexcel.convert import main
+from vocexcel.utils import EXCEL_FILE_ENDINGS
+
+__version__ = "0.1.0"
+
+
+def run_vocexcel(args=None):
+    main(args)
+
+
+def wrapper(args=None):
+    print("Running VocExcel wrapper")
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        "-v",
+        "--version",
+        help="The version of this wrapper of VocExel.",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "file_to_preprocess",
+        nargs="?",  # allow 0 or 1 file name as argument
+        type=Path,
+        help="The Excel file to preprocess and convert to a SKOS vocabulary.",
+    )
+
+    args_wrapper = parser.parse_args()
+
+    if args_wrapper.version:
+        print(f"{__version__}")
+    elif args_wrapper.file_to_preprocess:
+        if not args_wrapper.file_to_preprocess.suffix.lower().endswith(
+            tuple(EXCEL_FILE_ENDINGS)
+        ):
+            print("Files for preprocessing must end with .xlsx (Excel).")
+            sys.exit()
+        else:
+            print(f"Processing file {args_wrapper.file_to_preprocess}")
+            print("\nCalling VocExcel")
+            main(args)
+
+    return args_wrapper
+
+
+if __name__ == "__main__":
+    w = wrapper(sys.argv[1:])
