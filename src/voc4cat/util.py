a0f17a826ca4a3f90d17fd19fa6c28259826b7c6
David Linke <david.linke@catalysis.de>
2022-06-02 00:06:48 +0200

    Add node class to build trees (with tests)

---
+++ src/voc4cat/util.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+
+from copy import copy
+from itertools import chain
+from warnings import warn
+
+
+class Node:
+    """
+    Node class to build a linked tree of nodes.
+
+    The tree can be built from
+
+    - indented text (the indent-seperator can be adjusted)
+    - a dictionary with node-children key-value pairs
+    """
+
+    def __init__(self, indented_line, sep=" "):
+        self.children = []
+        split_line = indented_line.rstrip().split(sep)
+        self.level = len(split_line) - 1
+        self.text = split_line[self.level]
+        if len(sep) > 1 and self.text.startswith(sep[0]):
+            warn(f'Node "{self.text}": Incomplete separator "{sep}"?')
+
+    def add_children(self, nodes):
+        """Add more child nodes to node."""
+        if not nodes:
+            return
+        for node in nodes:
+            if self.level > node.level:
+                raise ValueError(
+                    f'Level of node "{node.text}" lower than of ' "root node."
+                )
+            if nodes[0].level > node.level:
+                raise ValueError(
+                    f'Level of node "{node.text}" lower than of ' "first node to add."
+                )
+        self.__add_children(nodes)
+
+    def __add_children(self, nodes):
+        childlevel = nodes[0].level
+        while nodes:
+            node = nodes.pop(0)
+            if node.level == childlevel:  # add node as a child
+                self.children.append(node)
+            elif (node.level - childlevel) == 1:  # grandchildren of last child
+                nodes.insert(0, node)
+                if self.children:
+                    self.children[-1].__add_children(nodes)
+            elif (node.level - childlevel) > 1:
+                raise ValueError(
+                    "Indentation inreases by more than one " f'level for "{node.text}"'
+                )
+            elif node.level <= self.level:  # sibling, no more children
+                nodes.insert(0, node)
+                return
+
+    def add_nodes_narrower(self, narrower):
+        """Add children and their level from narrower dict (non-recursive)."""
+        nodes = {}
+        stack = []
+        list_of_all_children = list(chain.from_iterable([v for v in narrower.values()]))
+
+        # Create nodes that are never present as child and store in nodes.
+        # Only these nodes are fully defined at this point.
+        for nd in list(narrower.keys()):
+            node = Node(nd)
+            node.level = self.level
+            if not narrower[nd] and nd not in list_of_all_children:
+                nodes[nd] = node
+                self.children.append(node)
+            else:
+                stack.append((nd, narrower[nd]))
+
+        # Process stack of nodes which are not fully defined. Use the defined
+        # nodes to add the children level by level.
+        level = 0
+        while stack:
+            all_children_defined = copy(stack)
+            to_add = []
+            for nd, v in stack:
+                all_children_defined = True
+                for c in v:
+                    if c not in nodes:
+                        all_children_defined = False
+                if all_children_defined:
+                    to_add.append((nd, v))
+
+            for nd, v in to_add:
+                node = Node(nd)
+                node.level = level + 1
+                for c in v:
+                    chnode = nodes[c]
+                    node.children.append(chnode)
+                if nd not in list_of_all_children:  # "root" level
+                    node.level = self.level
+                    self.children.append(node)
+                nodes[nd] = node
+                stack.remove((nd, v))
+            level += 1
+
+    def get_level(self, narrower, key, level=0):
+        """Return the depth level of a given node."""
+        for n, ch in narrower.items():
+            if key in ch:
+                return self.get_level(narrower, n, level + 1)
+        return level
+
+    def as_dict(self):
+        """Return tree of nodes as nested dictionary."""
+        if len(self.children) > 0:
+            return {
+                self.text: [
+                    node.as_dict()
+                    for node in sorted(self.children, key=lambda child: child.text)
+                ]
+            }
+        else:
+            return self.text
+
+    def as_indented_text(self, sep=None, out=None):
+        """Return tree of nodes as indented text."""
+        if out is None:
+            out = []
+        if len(self.children) > 0:
+            out.append(f"{self.level*sep}{self.text}")
+            for node in self.children:
+                node.as_indented_text(sep, out)
+            return out
+        else:
+            out.append(f"{self.level*sep}{self.text}")
+            return out
+
+    def as_narrower_dict(self, out=None):
+        """Return tree of nodes as flat dictionary of nodes with children."""
+        if out is None:
+            out = {}
+        if len(self.children) > 0:
+            out[self.text] = sorted([c.text for c in self.children])
+            for node in self.children:
+                node.as_narrower_dict(out)
+        else:
+            out[self.text] = []
+        return out
+
+
+# === Factory functions to build a tree of nodes ===
+
+def build_tree(text, sep=" "):
+    """Build tree of nodes from indented text."""
+    root = Node("root", sep=sep)
+    root.add_children([Node(line) for line in text.splitlines() if line.strip()])
+    return root
+
+
+def build_from_narrower(narrower):
+    """Build tree of nodes from narrower dictionary."""
+    roots = []
+    for k in narrower.keys():
+        # find which node(s) are not linked by any other
+        if k not in chain.from_iterable([v for v in narrower.values()]):
+            roots.append(k)
+    if len(roots) == 1:
+        root = Node(roots[0])
+        narrower_no_root = copy(narrower)
+        del narrower_no_root[roots[0]]
+    elif len(roots) < 1:
+        raise ValueError("No root found on root level.")
+    else:
+        raise ValueError("More than one node on root level found.")
+
+    root.add_nodes_narrower(narrower_no_root)
+    return root
---
+++ tests/test_util.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from voc4cat.util import Node, build_tree, build_from_narrower
+
+text1 = """
+a1
+ b1
+  c1
+   L1
+"""
+expected1 = {"root": [{"a1": [{"b1": [{"c1": ["L1"]}]}]}]}
+narrower1 = {"root": ["a1"], "a1": ["b1"], "b1": ["c1"], "c1": ["L1"], "L1": []}
+
+text2 = """
+a1
+a2
+ L1
+ L2
+ b1
+  L3
+  L4
+ b2
+  L5
+a3
+"""
+expected2 = {
+    "root": ["a1", {"a2": ["L1", "L2", {"b1": ["L3", "L4"]}, {"b2": ["L5"]}]}, "a3"]
+}
+
+narrower2 = {
+    "root": ["a1", "a2", "a3"],
+    "a1": [],
+    "a2": ["L1", "L2", "b1", "b2"],
+    "L1": [],
+    "L2": [],
+    "b1": ["L3", "L4"],
+    "L3": [],
+    "L4": [],
+    "b2": ["L5"],
+    "L5": [],
+    "a3": [],
+}
+
+
+def test_text1():
+    tree = build_tree(text1)
+    expected_text = """root
+a1
+..b1
+....c1
+......L1
+""".split()
+    # print("n=", tree.as_narrower_dict())
+    assert tree.as_dict() == expected1
+    assert tree.as_indented_text(sep="..") == expected_text
+    # assert tree.as_narrower_dict() == narrower1
+
+
+def test_text2():
+    tree = build_tree(text2)
+    # print(tree.as_dict())
+    expected_text = """root
+a1
+a2
+-L1
+-L2
+-b1
+--L3
+--L4
+-b2
+--L5
+a3
+""".split()
+    assert tree.as_dict() == expected2
+    assert tree.as_indented_text(sep="-") == expected_text
+    assert tree.as_narrower_dict() == narrower2
+
+
+def test_text2_reordered():
+    text2 = """
+a2
+ b2
+  L5
+ L2
+ L1
+ b1
+  L4
+  L3
+a3
+a1
+"""
+    tree = build_tree(text2)
+    assert tree.as_dict() == expected2
+    assert tree.as_narrower_dict() == narrower2
+
+
+def test_from_narrower():
+    assert build_from_narrower(narrower1).as_dict() == expected1
+    assert build_from_narrower(narrower2).as_dict() == expected2
+
+
+def test_order_narrower():
+    rev_narrower1 = dict(reversed(list(narrower1.items())))
+    assert build_from_narrower(rev_narrower1).as_dict() == expected1
+    rev_narrower2 = dict(reversed(list(narrower2.items())))
+    assert build_from_narrower(rev_narrower2).as_dict() == expected2
+
+
+def test_from_narrower_no_root():
+    n = {"a1": ["a2"], "a2": ["a1"]}
+    with pytest.raises(ValueError) as excinfo:
+        build_from_narrower(n)
+    assert "No root found on root level." in str(excinfo.value)
+
+
+def test_from_narrower_more_roots():
+    n = {"a1": [], "a2": []}
+    with pytest.raises(ValueError) as excinfo:
+        build_from_narrower(n)
+    assert "More than one node on root level found." in str(excinfo.value)
+
+
+def test_pure_tree():
+    tree = build_tree("", sep="x")
+    assert tree.children == []
+    assert tree.text == "root"
+    assert tree.level == 0
+
+
+def test_one_node():
+    text = "n1"
+    tree = build_tree(text)
+    expected = {"root": ["n1"]}
+    assert len(tree.children) == 1
+    assert tree.as_dict() == expected
+
+
+def test_bad_dedent():
+    text = " x1\nx2"
+    with pytest.raises(ValueError) as excinfo:
+        build_tree(text)
+    assert 'Level of node "x2" lower than of first node to add.' in str(excinfo.value)
+
+
+def test_root_higher_level():
+    tree = Node(" root")
+    with pytest.raises(ValueError) as excinfo:
+        tree.add_children([Node("x2")])
+    assert 'Level of node "x2" lower than of root node.' in str(excinfo.value)
+
+
+def test_bad_indent():
+    text = "x1\n  x2"
+    with pytest.raises(ValueError) as excinfo:
+        build_tree(text)
+    assert 'Indentation inreases by more than one level for "x2"' in str(excinfo.value)
+
+
+def test_non_matching_indent_warning():
+    tree = Node("root", sep="--")
+    with pytest.warns(UserWarning, match='Node "-n3": Incomplete separator "--"?'):
+        tree.add_children(
+            [Node("n1", sep="--"), Node("--n2", sep="--"), Node("---n3", sep="--")]
+        )
