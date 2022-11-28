299d5c45f52c10188d235684a77f4c8aad968490
David Linke <david.linke@catalysis.de>
2022-11-28 15:16:36 +0100

    Cleanup digraph code & bring its test coverage to 100%

--- src/voc4cat/util.py
+++ src/voc4cat/util.py
@@ -1,207 +1,9 @@
-# from copy import copy
-# from functools import total_ordering
-# from itertools import chain
+# -*- coding: utf-8 -*-
 from operator import itemgetter
 from warnings import warn
 
 import networkx as nx
 
-# @total_ordering
-# class Node:
-#     """
-#     Node class to build a linked tree of nodes.
-
-#     The tree can be built from
-
-#     - indented text (the indent-seperator can be adjusted)
-#     - a dictionary with node-children key-value pairs
-#     """
-
-#     def __init__(self, indented_line, sep=" "):
-#         self.children = []
-#         split_line = indented_line.rstrip().split(sep)
-#         self.level = len(split_line) - 1
-#         self.text = split_line[self.level]
-#         if sep is not None and len(sep) > 1 and self.text.startswith(sep[0]):
-#             warn(f'Node "{self.text}": Incomplete separator "{sep}"?')
-
-#     def add_children(self, nodes):
-#         """Add more child nodes to node."""
-#         if not nodes:
-#             return
-#         for node in nodes:
-#             if self.level > node.level:
-#                 raise ValueError(
-#                     f'Level of node "{node.text}" lower than of ' "root node."
-#                 )
-#             if nodes[0].level > node.level:
-#                 raise ValueError(
-#                     f'Level of node "{node.text}" lower than of ' "first node to add."
-#                 )
-#         # Make a copy to avoid changing nodes.
-#         self.__add_children(list(nodes))
-
-#     def __add_children(self, nodes):
-#         childlevel = nodes[0].level
-#         while nodes:
-#             node = nodes.pop(0)
-#             if node.level == childlevel:  # add node as a child
-#                 self.children.append(node)
-#             elif (node.level - childlevel) == 1:  # grandchildren of last child
-#                 nodes.insert(0, node)
-#                 if self.children:
-#                     self.children[-1].__add_children(nodes)
-#             elif (node.level - childlevel) > 1:
-#                 raise ValueError(
-#                     "Indentation inreases by more than one " f'level for "{node.text}"'
-#                 )
-#             elif node.level <= self.level:  # sibling, no more children
-#                 nodes.insert(0, node)
-#                 return
-
-#     def add_nodes_narrower(self, narrower):
-#         """Add children and their level from narrower dict."""
-#         nodes = {}
-#         stack = []
-#         list_of_all_children = list(chain.from_iterable(list(narrower.values())))
-
-#         # Check if all children are also key in narrower.
-#         for ch in list_of_all_children:
-#             if ch not in narrower.keys():
-#                 raise ValueError(f'Child "{ch}" is not defined.')
-
-#         # Create nodes that are never present as child and store in nodes.
-#         # Only these nodes are fully defined at this point.
-#         for nd in list(narrower.keys()):
-#             node = Node(nd)
-#             node.level = self.level
-#             if not narrower[nd] and nd not in list_of_all_children:
-#                 nodes[nd] = node
-#                 self.children.append(node)
-#             else:
-#                 stack.append((nd, narrower[nd]))
-
-#         # Process stack of nodes which are not fully defined. Use the defined
-#         # nodes to add the children level by level.
-#         while stack:
-#             to_add = []
-#             for nd, v in stack:
-#                 all_children_defined = True
-#                 for c in v:
-#                     if c not in nodes:
-#                         all_children_defined = False
-#                 if all_children_defined:
-#                     to_add.append((nd, v))
-
-#             for nd, v in to_add:
-#                 node = Node(nd)
-#                 node.level = self.get_level(narrower, nd, self.level)
-#                 for c in v:
-#                     chnode = nodes[c]
-#                     node.children.append(chnode)
-#                 if nd not in list_of_all_children:  # "root" level
-#                     node.level = self.level
-#                     self.children.append(node)
-#                 nodes[nd] = node
-#                 stack.remove((nd, v))
-
-#     def get_level(self, narrower, key, level=0):
-#         """Return the depth level of a given node."""
-#         for n, ch in narrower.items():
-#             if key in ch:
-#                 return self.get_level(narrower, n, level + 1)
-#         return level
-
-#     def as_dict(self):
-#         """Return tree of nodes as nested dictionary."""
-#         if self.children:
-#             return {
-#                 self.text: [
-#                     node.as_dict()
-#                     for node in sorted(self.children, key=lambda child: child.text)
-#                 ]
-#             }
-#         return self.text
-
-#     def as_indented_text(self, sep=" ", out=None):
-#         """Return tree of nodes as indented text."""
-#         if out is None:
-#             out = []
-#         for text, level in self.as_level_dict().items():
-#             out.append(f"{level*sep}{text}")
-#         return out
-
-#     def as_level_dict(self, out=None):
-#         """Return hierarchically sorted list of of all nodes in tree with level."""
-#         if out is None:
-#             out = {}
-#         if self.children:
-#             out[self.text] = self.level
-#             for node in sorted(self.children, key=lambda t: t.text):
-#                 node.as_level_dict(out)
-#             return out
-#         out[self.text] = self.level
-#         return out
-
-#     def as_narrower_dict(self, out=None):
-#         """Return tree of nodes as flat dictionary of nodes with children."""
-#         if out is None:
-#             out = {}
-#         if self.children:
-#             out[self.text] = sorted([c.text for c in self.children])
-#             for node in self.children:
-#                 node.as_narrower_dict(out)
-#         else:
-#             out[self.text] = []
-#         return out
-
-#     def __eq__(self, other):
-#         return (self.text, self.level, self.children) == (
-#             other.text,
-#             other.level,
-#             other.children,
-#         )
-
-#     def __lt__(self, other):
-#         return (self.text, self.level, self.children) < (
-#             other.text,
-#             other.level,
-#             other.children,
-#         )
-
-
-# === Factory functions to build a tree of nodes ===
-
-
-# def build_tree(text, sep=" "):
-#     """Build tree of nodes from indented text."""
-#     root = Node("root", sep=sep)
-#     root.add_children([Node(line) for line in text.splitlines() if line.strip()])
-#     return root
-
-
-# def build_from_narrower(narrower):
-#     """Build tree of nodes from narrower dictionary."""
-#     roots = []
-#     for k in narrower.keys():
-#         # find which node(s) are not linked by any other
-#         if k not in chain.from_iterable(list(narrower.values())):
-#             roots.append(k)
-#     if not roots:
-#         raise ValueError("No root found on root level.")
-#     if len(roots) == 1:
-#         root = Node(roots[0])
-#         narrower_no_root = copy(narrower)
-#         narrower_no_root.pop(roots[0])
-#     else:
-#         root = Node("root")
-#         narrower_no_root = copy(narrower)
-
-#     root.add_nodes_narrower(narrower_no_root)
-#     return root
-
-
-# === NetworkX implementation ===============================================
 
 def _get_edges(text_with_level, base_level):  # noqa: WPS231
     edges = []
@@ -209,13 +11,15 @@ def _get_edges(text_with_level, base_level):  # noqa: WPS231
     for concept, level in text_with_level:
         if level not in level_parent_map:  # no parent stored for this level
             level_parent_map[level] = concept
-        if level == max(level_parent_map):  # a new possible parent
+        if level == max(level_parent_map.keys()):  # a new possible parent
             level_parent_map[level] = concept
             if level > base_level:
                 edges.append((level_parent_map[level - 1], concept))
-        elif level < max(level_parent_map):  # back to lower level
+        else:  # back to lower level
             # remove higher-level parents and register new parent for this level
-            level_parent_map = {lvl: cpt for lvl, cpt in level_parent_map.items() if lvl <= level}
+            level_parent_map = {
+                lvl: cpt for lvl, cpt in level_parent_map.items() if lvl <= level
+            }
             level_parent_map[level] = concept
             if level > base_level:
                 edges.append((level_parent_map[level - 1], concept))
@@ -237,15 +41,22 @@ def dag_from_indented_text(text, sep=" "):
             nodes.append(concept)
         if sep is not None and len(sep) > 1 and concept.startswith(sep[0]):
             warn(f'Line "{concept}": Incomplete separator "{sep}"?')
+        if text_with_level and (level - 1) > text_with_level[-1][1]:
+            raise ValueError(
+                f'Indentation inreases by more than one level for "{concept}".'
+            )
         text_with_level.append((concept, level))
 
-    _, base_level = min(text_with_level, key=itemgetter(1))
-
-    # Check if first line is at base level.
     if text_with_level:
+        _, base_level = min(text_with_level, key=itemgetter(1))
+        # Check if first line is at base level.
         concept, level = text_with_level[0]
         if base_level != level:
-            raise ValueError(f'First line "{concept}" must be at lowest level.')
+            raise ValueError(
+                f'First line "{concept}" must be at lowest indentation level.'
+            )
+    else:
+        base_level = 0
 
     edges = _get_edges(text_with_level, base_level)
 
@@ -263,50 +74,11 @@ def dag_to_indented_text(termdag, sep=" "):
         out.append(level * sep + node)
     return out
 
-# def _print_indented(sl, root_node, level=0, sep="  "):
-#     successors = sl.pop(root_node)
-#     print(level * sep + root_node)
-#     for sn in successors:
-#         if sn in sl:
-#             _print_indented(sl, sn, level=level + 1, sep=sep)
-#         else:
-#             print((level + 1) * sep + sn)
-
-
-# def dag_to_indent(termgraph, sep="  "):
-#     subgraphs = [
-#         termgraph.subgraph(c).copy()
-#         for c in nx.connected_components(termgraph.to_undirected())
-#     ]
-#     for subgraph in subgraphs:
-#         print(f"\nsubgraph: {subgraph}")
-#         broken_edges = []
-#         for cycle in nx.simple_cycles(subgraph):
-#             edge = _break_cycles(subgraph, cycle)
-#             if edge:
-#                 broken_edges.append(edge)
-
-#         # We need a clean subgraph without cylces for creating an indented tree.
-#         subgraph_clean = subgraph.copy()
-#         subgraph_clean.remove_edges_from(broken_edges)
-#         roots = [node for node, degree in subgraph_clean.in_degree() if degree == 0]
-
-#         for root in roots:
-#             # successors in breadth-first-search
-#             succ_bfs = dict(nx.bfs_successors(subgraph_clean, root))
-#             _print_indented(succ_bfs, root, level=0)
-
-#         # Add broken_edges
-#         for edge in broken_edges:
-#             start_node, end_node = edge
-#             print(start_node)
-#             print(sep + end_node)
-
 
 def _break_cycles(dag, edges_cycle):
     # print(f"-> has cycle: {edges_cycle} - len={len(edges_cycle)}")
     if len(edges_cycle) < 3:
-        print(f"-> Small unbreakable cycle! {edges_cycle}")
+        warn(f'Small unbreakable cycle detected: "{edges_cycle}"')
         return ()
     # find edge to break
     deg_out = dict(dag.out_degree)
@@ -335,7 +107,7 @@ def _node_levels(sl, root_node, level=0, sep="  ", out=None):
 
 
 def dag_to_node_levels(termgraph, baselevel=0):
-    """Build tree represenation of directed graph breaking cycles as needed."""
+    """Build tree representation of directed graph breaking cycles as needed."""
     subgraphs = [
         termgraph.subgraph(sgc).copy()
         for sgc in nx.connected_components(termgraph.to_undirected())
@@ -374,7 +146,13 @@ def dag_from_narrower(narrower):
     edges = []
     for concept, children in narrower.items():
         for child in children:
+            # check for undefined children
+            if child not in nodes:
+                raise ValueError(
+                    f'Concept "{child}" needs to defined if use as narrower concept.'
+                )
             edges.append((concept, child))
+
     dag = nx.DiGraph()
     dag.add_nodes_from(nodes)
     dag.add_edges_from(edges)
@@ -385,31 +163,5 @@ def dag_to_narrower(dag):
     """Build children representation form networkx directed graph."""
     narrower = {}
     for node in dag.nodes:
-        narrower[node] = list(dag.successors(node))
+        narrower[node] = sorted(dag.successors(node))
     return narrower
-
-
-if __name__ == "__main__":
-    # Show roundtrip: indented text -> dag -> indented text
-    text = """
-ex_1
-    ex_2
-    ex_3
-        ex_4
-        ex_5
-    ex_6
-        ex_7
-ex_9
-    ex_7
-ex_4
-    ex_1
-ex_8
-ex_10
-    ex_11
-"""
-    print(f"Input: {text}")
-    termdag = dag_from_indented_text(text, sep="    ")
-    print("New indented output:")
-    dag_to_indented_text(termdag)
-    print("Children representation:")
-    print(dag_to_narrower_dict(termdag))
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from voc4cat.util import dag_from_indented_text, dag_to_indented_text, dag_from_narrower, dag_to_narrower
-
+from voc4cat.util import (
+    dag_from_indented_text,
+    dag_from_narrower,
+    dag_to_indented_text,
+    dag_to_narrower,
+    dag_to_node_levels,
+)
 
 text1 = """
 a1
@@ -28,8 +33,14 @@ a3
 """
 nodes2 = ["a1", "a2", "L1", "L2", "b1", "L3", "L4", "b2", "L5", "a3"]
 edges2 = {
-    ("a2", "L1"), ("a2", "L2"), ("a2", "L2"), ("a2", "b1"),
-    ("a2", "b2"), ("b1", "L3"), ("b1", "L4"), ("b2", "L5"),
+    ("a2", "L1"),
+    ("a2", "L2"),
+    ("a2", "L2"),
+    ("a2", "b1"),
+    ("a2", "b2"),
+    ("b1", "L3"),
+    ("b1", "L4"),
+    ("b2", "L5"),
 }
 narrower2 = {
     "a1": [],
@@ -46,21 +57,21 @@ narrower2 = {
 
 
 def test_text1():
-    tree = dag_from_indented_text(text1)
+    dag = dag_from_indented_text(text1)
     expected_text = """
 a1
 ..b1
 ....c1
 ......L1
 """.strip().split()
-    assert list(tree.nodes) == nodes1
-    assert set(tree.edges) == edges1
-    assert dag_to_narrower(tree) == narrower1
-    assert dag_to_indented_text(tree, sep="..") == expected_text
+    assert list(dag.nodes) == nodes1
+    assert set(dag.edges) == edges1
+    assert dag_to_narrower(dag) == narrower1
+    assert dag_to_indented_text(dag, sep="..") == expected_text
 
 
 def test_text2():
-    tree = dag_from_indented_text(text2)
+    dag = dag_from_indented_text(text2)
     expected_text = """
 a1
 a2
@@ -73,10 +84,10 @@ a2
 --L5
 a3
 """.strip().split()
-    assert list(tree.nodes) == nodes2
-    assert set(tree.edges) == edges2
-    assert dag_to_narrower(tree) == narrower2
-    assert dag_to_indented_text(tree, sep="-") == expected_text
+    assert list(dag.nodes) == nodes2
+    assert set(dag.edges) == edges2
+    assert dag_to_narrower(dag) == narrower2
+    assert dag_to_indented_text(dag, sep="-") == expected_text
 
 
 def test_text2_reordered():
@@ -92,65 +103,31 @@ a2
 a3
 a1
 """
-    tree = dag_from_indented_text(text2_reordered)
-    assert set(tree.nodes) == set(nodes2)
-    assert set(tree.edges) == edges2
-    #assert dag_to_narrower(tree) == narrower2
-    assert dag_to_indented_text(tree) == text2_reordered.strip().split("\n")
+    dag = dag_from_indented_text(text2_reordered)
+    assert set(dag.nodes) == set(nodes2)
+    assert set(dag.edges) == edges2
+    assert dag_to_narrower(dag) == narrower2
+    assert dag_to_indented_text(dag) == text2_reordered.strip().split("\n")
 
 
 def test_from_narrower():
-    assert dag_from_narrower(narrower1).as_dict() == expected1
-    assert dag_from_narrower(narrower2).as_dict() == expected2
+    dag1 = dag_from_narrower(narrower1)
+    assert set(dag1.nodes) == set(nodes1)
+    assert set(dag1.edges) == edges1
+    dag2 = dag_from_narrower(narrower2)
+    assert set(dag2.nodes) == set(nodes2)
+    assert set(dag2.edges) == edges2
 
 
 def test_order_narrower():
     rev_narrower1 = dict(reversed(list(narrower1.items())))
-    assert dag_from_narrower(rev_narrower1).as_dict() == expected1
+    dag1 = dag_from_narrower(rev_narrower1)
+    assert set(dag1.nodes) == set(nodes1)
+    assert set(dag1.edges) == edges1
     rev_narrower2 = dict(reversed(list(narrower2.items())))
-    assert dag_from_narrower(rev_narrower2).as_dict() == expected2
-
-
-def test_from_narrower_no_root():
-    n = {"a1": ["a2"], "a2": ["a1"]}
-    with pytest.raises(ValueError) as excinfo:
-        dag_from_narrower(n)
-    assert "No root found on root level." in str(excinfo.value)
-
-
-# def test_from_narrower_more_roots():
-#     n = {"a1": [], "a2": []}
-#     with pytest.raises(ValueError) as excinfo:
-#         dag_from_narrower(n)
-#     assert "More than one node on root level found." in str(excinfo.value)
-
-
-def test_narrower3():
-    n = {
-        "ex:1": ["ex:2", "ex:3", "ex:6"],
-        "ex:2": [],
-        "ex:3": ["ex:4", "ex:5"],
-        "ex:4": [],
-        "ex:5": [],
-        "ex:6": ["ex:7"],
-        "ex:7": [],
-        "ex:8": [],
-        "ex:9": [],
-    }
-    tree = dag_from_narrower(n)
-    expected = {
-        "root": 0,
-        "ex:1": 0,
-        "ex:2": 1,
-        "ex:3": 1,
-        "ex:4": 2,
-        "ex:5": 2,
-        "ex:6": 1,
-        "ex:7": 2,
-        "ex:8": 0,
-        "ex:9": 0,
-    }
-    assert tree.as_level_dict() == expected
+    dag2 = dag_from_narrower(rev_narrower2)
+    assert set(dag2.nodes) == set(nodes2)
+    assert set(dag2.edges) == edges2
 
 
 def test_narrower_same_concept_in_two_trees():
@@ -160,127 +137,140 @@ def test_narrower_same_concept_in_two_trees():
         "ex:3": [],
         "ex:4": ["ex:3"],
     }
-    tree = dag_from_narrower(n)
-    expected = {
-        "root": 0,
-        "ex:1": 0,
-        "ex:2": 1,
-        "ex:3": 2,
-        "ex:4": 0,
-        "ex:3": 1,
+    dag = dag_from_narrower(n)
+    expected = [
+        ("ex:1", 0),
+        ("ex:2", 1),
+        ("ex:3", 2),
+        ("ex:4", 0),
+        ("ex:3", 1),
+    ]
+    assert dag_to_node_levels(dag) == expected
+
+
+def test_narrower_with_cylce():
+    narrower = {
+        "ex:1": ["ex:2", "ex:3", "ex:6"],
+        "ex:2": [],
+        "ex:3": ["ex:4", "ex:5"],
+        "ex:4": ["ex:1"],
+        "ex:5": [],
+        "ex:6": ["ex:7"],
+        "ex:7": [],
+        "ex:9": ["ex:7"],
+        "ex:8": [],
+        "ex:10": ["ex:11"],
+        "ex:11": [],
     }
-    assert tree.as_level_dict() == expected
+    expected_text = """
+ex:1
+  ex:2
+  ex:3
+    ex:4
+    ex:5
+  ex:6
+    ex:7
+ex:9
+  ex:7
+ex:4
+  ex:1
+ex:8
+ex:10
+  ex:11
+""".strip().split(
+        "\n"
+    )
+    expected_node_levels = [
+        ("ex:1", 0),
+        ("ex:2", 1),
+        ("ex:3", 1),
+        ("ex:4", 2),
+        ("ex:5", 2),
+        ("ex:6", 1),
+        ("ex:7", 2),
+        ("ex:9", 0),
+        ("ex:7", 1),
+        ("ex:4", 0),
+        ("ex:1", 1),
+        ("ex:8", 0),
+        ("ex:10", 0),
+        ("ex:11", 1),
+    ]
+    dag = dag_from_narrower(narrower)
+    assert dag_to_node_levels(dag) == expected_node_levels
+    assert dag_to_indented_text(dag, sep="  ") == expected_text
 
 
-# def test_narrower_repeats():
-#     n = {
-# 'ex:1': ['ex:2', 'ex:3', 'ex:6'],
-# 'ex:2': [],
-# 'ex:3': ['ex:4', 'ex:5'],
-# 'ex:4': ['ex:1'],
-# 'ex:5': [],
-# 'ex:6': ['ex:7'],
-# 'ex:7': [],
-# 'ex:8': [],
-# 'ex:9': ['ex:7'],
-# 'ex:10': ['ex:11'],
-# 'ex:11': [],
-#     }
-#     tree = dag_from_narrower(n)
-#     expected = {
-#         'root': 0,
-#         'ex:1': 0,
-#         'ex:2': 1,
-#         'ex:3': 1,
-#         'ex:4': 2,
-#         'ex:5': 2,
-#         'ex:6': 1,
-#         'ex:7': 2,
-#         'ex:8': 0,
-#         'ex:9': 0,
-#         'ex:7': 1,
-#         'ex:4': 0,
-#         'ex:1': 1,
-#     }
-#     assert tree.as_level_dict() == expected
-
-
-def test_from_narrower_more_roots():
+def test_from_narrower_multiple_root_nodes():
     n = {"a1": [], "a2": []}
-    tree = dag_from_narrower(n)
-    expected = {"root": ["a1", "a2"]}
-    assert tree.as_dict() == expected
+    dag = dag_from_narrower(n)
+    assert list(dag.nodes) == list(n.keys())
+    assert list(dag.edges) == []
 
 
-def test_undefined_childURI():
+def test_redefinition_within_text():
+    n = """
+a1
+a2
+a2
+-a1
+"""
+    dag = dag_from_indented_text(n, sep="-")
+    assert list(dag.nodes) == ["a1", "a2"]
+    assert list(dag.edges) == [("a2", "a1")]
+
+
+def test_undefined_child():
     n = {"a1": [], "a2": ["c"]}
     with pytest.raises(ValueError) as excinfo:
         dag_from_narrower(n)
-    assert 'Child "c" is not defined.' in str(excinfo.value)
+    assert 'Concept "c" needs to defined if use as narrower concept.' in str(
+        excinfo.value
+    )
 
 
-def test_pure_tree():
-    tree = dag_from_indented_text("", sep="x")
-    assert tree.children == []
-    assert tree.text == "root"
-    assert tree.level == 0
+def test_empty_text():
+    dag = dag_from_indented_text("", sep="x")
+    assert list(dag.nodes) == []
+    assert list(dag.edges) == []
 
 
 def test_one_node():
     text = "n1"
-    tree = dag_from_indented_text(text)
-    expected = {"root": ["n1"]}
-    assert len(tree.children) == 1
-    assert tree.as_dict() == expected
+    dag = dag_from_indented_text(text)
+    assert list(dag.nodes) == ["n1"]
+    assert list(dag.edges) == []
 
 
 def test_none_as_sep():
     text = "n1"
-    tree = dag_from_indented_text(text, sep=None)
-    expected = {"root": ["n1"]}
-    assert len(tree.children) == 1
-    assert tree.as_dict() == expected
+    dag = dag_from_indented_text(text, sep=None)
+    assert list(dag.nodes) == ["n1"]
+    assert list(dag.edges) == []
 
 
 def test_bad_dedent():
     text = " x1\nx2"
     with pytest.raises(ValueError) as excinfo:
         dag_from_indented_text(text)
-    assert 'Level of node "x2" lower than of first node to add.' in str(excinfo.value)
-
-
-def test_root_higher_level():
-    tree = Node(" root")
-    with pytest.raises(ValueError) as excinfo:
-        tree.add_children([Node("x2")])
-    assert 'Level of node "x2" lower than of root node.' in str(excinfo.value)
+    assert 'First line "x1" must be at lowest indentation level.' in str(excinfo.value)
 
 
 def test_bad_indent():
-    text = "x1\n  x2"
+    text = "x1\n--x2"
     with pytest.raises(ValueError) as excinfo:
-        dag_from_indented_text(text)
+        dag_from_indented_text(text, sep="-")
     assert 'Indentation inreases by more than one level for "x2"' in str(excinfo.value)
 
 
 def test_non_matching_indent_warning():
-    tree = Node("root", sep="--")
-    with pytest.warns(UserWarning, match='Node "-n3": Incomplete separator "--"?'):
-        tree.add_children(
-            [Node("n1", sep="--"), Node("--n2", sep="--"), Node("---n3", sep="--")]
-        )
+    text = "n1\n--n2\n---n3"
+    with pytest.warns(UserWarning, match='Line "-n3": Incomplete separator "--"?'):
+        dag_from_indented_text(text, sep="--")
 
 
-def test_equality():
-    n1 = Node("n1")
-    n2 = Node(" n2")
-    assert n1 != n2
-    n1.level = 1
-    n2.text = "n1"
-    assert n1 == n2
-
-
-def test_order():
-    n1 = Node("n1")
-    n2 = Node("n2")
-    assert n1 < n2
+def test_small_cylce_warning():
+    n = {"a1": ["a2"], "a2": ["a1"]}
+    dag = dag_from_narrower(n)
+    with pytest.warns(UserWarning, match="Small unbreakable cycle detected: "):
+        dag_to_node_levels(dag)
