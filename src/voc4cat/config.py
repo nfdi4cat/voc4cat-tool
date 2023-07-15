dd9eec50681637c2d9b59f303bc2c1bc3b331dc1
David Linke <david.linke@catalysis.de>
2023-07-15 13:58:21 +0200

    Use pydantic model for config read from idranges.toml
    This also changes the structure of the config. All vocabularies
    are stored under a common namespace "vocabs".

--- src/voc4cat/config.py
+++ src/voc4cat/config.py
@@ -1,9 +1,12 @@
 # This module is used to share the configuration in across voc4cat.
 
+import logging
+import os
 import sys
 from pathlib import Path
 
 from curies import Converter
+from pydantic import AnyHttpUrl, BaseModel
 from rdflib import Graph
 from rdflib.namespace import NamespaceManager
 
@@ -12,16 +15,13 @@ if sys.version_info >= (3, 11):
 else:
     import tomli as tomllib
 
-VOCAB_TITLE = ""
-CONFIG_FILE = Path("idranges.toml").resolve()
+logger = logging.getLogger(__name__)
 
+# === Configuration that is not imported from idranges.toml ===
 
-def load_config(fp: Path):
-    with fp.open(mode="rb") as fp:
-        return tomllib.load(fp)
-
-
-idranges = load_config(CONFIG_FILE) if CONFIG_FILE.exists() else {}
+VOCAB_TITLE: str = ""
+CONFIG_FILE: Path = Path("idranges.toml").resolve()
+CI_RUN: bool = os.getenv("GITHUB_ACTIONS")
 
 namespace_manager = NamespaceManager(Graph())
 # Initialize curies-converter with default namespace of rdflib.Graph
@@ -29,4 +29,40 @@ curies_converter = Converter.from_prefix_map(
     {prefix: str(url) for prefix, url in namespace_manager.namespaces()}
 )
 
-# TODO Use pydantic for validation of idranges.toml content or https://gitlab.com/sscherfke/typed-settings
+
+# === Configuration imported from idranges.toml stored as pydantic model ===
+
+
+class Checks(BaseModel):
+    no_delete: bool
+
+
+class IdrangeItem(BaseModel):
+    first_id: int
+    last_id: int
+    gh_username: str
+    orcid: str
+    organisation_ror_id: str
+
+
+class Vocab(BaseModel):
+    id_length: int
+    checks: Checks
+    prefix_map: dict[str, AnyHttpUrl]
+    idrange: list[IdrangeItem]
+
+
+class Config(BaseModel):
+    single_vocab: bool = False
+    vocabs: dict[str, Vocab] = {}
+
+
+def load_config():
+    with CONFIG_FILE.open(mode="rb") as fp:
+        conf = tomllib.load(fp)
+    return Config(**conf)
+
+
+idranges = load_config() if CONFIG_FILE.exists() else Config()
+
+logger.debug("Config imported from %s", CONFIG_FILE)
