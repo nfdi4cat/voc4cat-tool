a5413ac26d377cbf73874ebe7006f9d03394f080
David Linke <david.linke@catalysis.de>
2026-08-27 00:49:52 +0200

    Type gh_index module

--- src/voc4cat/gh_index.py
+++ src/voc4cat/gh_index.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 from importlib.metadata import version
 from pathlib import Path
 
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, Template
 
 from voc4cat import config
 from voc4cat.checks import Voc4catError
@@ -18,20 +18,20 @@ STYLE_DIR = TEMPLATES_DIR
 
 
 class IndexPage:
-    def __init__(self, vocpath=None):
+    def __init__(self, vocpath: Path | None = None) -> None:
         self.METADATA = {}
         self.METADATA["title"] = "Index of vocabulary versions"
         self.vocpath = Path(".") if vocpath is None else Path(vocpath)
-        self.vocabs = []
-        self.vocab_data = defaultdict(dict)
-        self.tags = []
+        self.vocabs: list[str] = []
+        self.vocab_data: defaultdict[str, dict[str, str]] = defaultdict(dict)
+        self.tags: list[str] = []
 
-    def _load_template(self, template_file):
+    def _load_template(self, template_file: str) -> Template:
         return Environment(
             loader=FileSystemLoader(TEMPLATES_DIR), autoescape=True
         ).get_template(template_file)
 
-    def get_version_data(self):
+    def get_version_data(self) -> None:
         cmd = ["git", "-C", str(self.vocpath), "tag", "--list", "v[0-9]*-[0-9]*-[0-9]*"]
         logger.debug("Running cmd: %s", " ".join(cmd))
         outp = subprocess.run(cmd, capture_output=True, check=False)  # noqa: S603
@@ -59,14 +59,14 @@ class IndexPage:
                 self.vocab_data[voc][tag] = base_url + f"/{tag}"
                 logger.debug('Adding tag "%s" to index.html', tag)
 
-    def _make_versions(self):
+    def _make_versions(self) -> str:
         return self._load_template("vocabularies.html").render(
             tags=sorted(self.tags, reverse=True),
             vocabularies=self.vocab_data,
             has_releases=bool(self.tags),
         )
 
-    def _make_document(self):
+    def _make_document(self) -> str:
         with open(STYLE_DIR / "pylode.css") as f:
             css = f.read()
 
@@ -80,11 +80,11 @@ class IndexPage:
             voc4cat_version=version("voc4cat").split("+")[0],
         )
 
-    def generate_document(self):
+    def generate_document(self) -> str:
         return self._make_document()
 
 
-def build_multirelease_index(voc_path, out_path):
+def build_multirelease_index(voc_path: Path, out_path: Path) -> None:
     # load config
     conf = voc_path / "idranges.toml"
     if conf.exists():
